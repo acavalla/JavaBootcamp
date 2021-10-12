68130c741c3b4988baf1643693cba84066a508f8
acavalla <anna.cavalla@gmail.com>
2021-10-12 17:32:42 +0100

    Added executable jar file and built Calculator.app

--- 02_Calculator/src/main/java/bootcamp/app/CalculatorApp.java
+++ 02_Calculator/src/main/java/bootcamp/app/CalculatorApp.java
@@ -1,12 +1,18 @@
 package bootcamp.app;
 
+import bootcamp.calculator.Calculator;
+import bootcamp.data.Params;
+
+import java.math.BigDecimal;
+
 /**
  * TODO: The application should be able to handle 3 arguments (first number, second number, operation).
  * The application should write the results to the standard output.
  */
 public class CalculatorApp {
-    public static void main(final String[] args) {
-
-
+    public static void main(String[] args) {
+        Params params = new Params(new BigDecimal(args[0]), new BigDecimal(args[1]), args[2]);
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.calculate(params));
     }
 }
